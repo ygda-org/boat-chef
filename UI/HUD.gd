1f9a8343579ca57f4bf89de7011f18956b0f423f
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-07-11 15:34:24 -0400

    white fruit

--- UI/HUD.gd
+++ UI/HUD.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @onready var label_blue = $GridContainer/LabelBlue
-@onready var label_brown = $GridContainer/LabelBrown
+@onready var label_white = $GridContainer/LabelWhite
 @onready var label_red = $GridContainer/LabelRed
 @onready var label_purple = $GridContainer/LabelPurple
 @onready var label_yellow = $GridContainer/LabelYellow
@@ -16,7 +16,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	label_blue.text = str(GameState.inventory[0])
-	label_brown.text = str(GameState.inventory[1])
+	label_white.text = str(GameState.inventory[1])
 	label_red.text = str(GameState.inventory[2])
 	label_purple.text = str(GameState.inventory[3])
 	label_yellow.text = str(GameState.inventory[4])
