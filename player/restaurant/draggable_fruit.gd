e64e42bc7025cb53da590b8b35069a206ed41863
MrPuffyWuff <jasper13.com@gmail.com>
2026-07-11 15:32:46 -0400

    Fruit Throwing Modification
    fruit sticks to the mouse faster

--- player/restaurant/draggable_fruit.gd
+++ player/restaurant/draggable_fruit.gd
@@ -8,6 +8,9 @@ var mouse = false
 
 const FORCE_STRENGTH = 10000
 
+var spring_constant: float = 33
+var damping_constant: float = 5
+
 func _ready():
 	$Sprite2D.texture = texture_to_set
 
@@ -19,7 +22,14 @@ func _process(delta):
 	if grabbed:
 		var force = FORCE_STRENGTH*(get_global_mouse_position()-global_position).normalized()*delta
 		force *= (get_global_mouse_position()-global_position).length()/200
-		apply_force(force)
+		
+		var displacement_to_mouse = get_global_mouse_position() - global_position
+
+		var spring_force = displacement_to_mouse * spring_constant
+		var damping_force = -linear_velocity * damping_constant
+			
+		apply_force(spring_force + damping_force)
+		
 	else:
 		apply_force(Vector2(0, 100))
 
