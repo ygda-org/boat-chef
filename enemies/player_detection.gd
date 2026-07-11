d4477f537f9ca90ef5f6043bca564bba3a5feab7
CookieFinder23 <190116541+CookieFinder23@users.noreply.github.com>
2026-07-11 07:29:07 -0400

    Update player_detection.gd

--- enemies/player_detection.gd
+++ enemies/player_detection.gd
@@ -1,7 +1,7 @@
 extends Area2D
 @export var size: int
 
-var player_position: Vector2
+var player_position
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,3 +10,5 @@ func _ready() -> void:
 func _physics_process(_delta: float) -> void:
 	if has_overlapping_bodies(): # Can only overlap with boat
 		player_position = get_overlapping_bodies()[0].global_position
+	else:
+		player_position = null
