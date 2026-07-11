d3fc2637cea8b266f27749e8a5c53cd58d1392dc
CookieFinder23 <190116541+CookieFinder23@users.noreply.github.com>
2026-07-11 07:12:21 -0400

    Update boat.gd

--- player/boat.gd
+++ player/boat.gd
@@ -6,6 +6,9 @@ const MAX_SPEED = 200
 
 const CAMERA_ZOOM_DISTANCE = 150
 
+const ROTATE_SPEED = 0.8
+var sprite_rotation: float = 0.0
+var target_rotation: float = 0.0
 
 func _physics_process(delta):
 	var dir = Input.get_vector("left", "right", "up", "down")
@@ -14,4 +17,10 @@ func _physics_process(delta):
 		velocity = (velocity + dir * ACCELERATION * delta).limit_length(MAX_SPEED)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, DECELERATION * delta)
+	target_rotation = velocity.angle()
+	sprite_rotation = lerp_angle(sprite_rotation, target_rotation, 1 - pow(delta, 1 - ROTATE_SPEED))
+	
+	# make logic for picking which frame to draw from sprite rotation
+	# maybe rework velocity system to make it based off rotation like tank controls? idk
+	
 	move_and_slide()
