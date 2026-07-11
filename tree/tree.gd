41da0c71be03ca89f3cce7a2a99f52b2ab847a2e
Theophylaktos <91230260+theophylaktos@users.noreply.github.com>
2026-07-11 13:21:52 -0400

    Pick fruit SFX

--- tree/tree.gd
+++ tree/tree.gd
@@ -8,12 +8,15 @@ var fruit = (randi_range(0,4))
 
 const TREE_PARTICLES = preload("uid://blfwcc7chqy1h")
 
+@onready var sfx_player = $SfxPlayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	
 	# Make the E-Prompt visible/invisible
 	if boat_in_area == true:
 		control.visible = true
@@ -27,6 +30,9 @@ func _process(_delta):
 			get_parent().add_child(particles)
 			particles.global_position = global_position
 			particles.emitting = true
+			visible = false
+			sfx_player.playSound()
+			await sfx_player.finished
 			queue_free()
 
 func _on_area_2d_body_entered(_body):
