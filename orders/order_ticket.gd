a2555df482f87fbf1a849d9cbb414272bef9af79
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-07-11 07:34:10 -0400

    quick push

--- orders/order_ticket.gd
+++ orders/order_ticket.gd
@@ -6,7 +6,7 @@ extends Control
 @onready var label_white = $GridContainer/LabelWhite
 @onready var label_yellow = $GridContainer/LabelYellow
 
-@export var order_number = 0
+var order_resource
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,8 +15,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	label_blue.text = str(GameState.orders[order_number][0])
-	label_brown.text = str(GameState.orders[order_number][1])
-	label_red.text = str(GameState.orders[order_number][2])
-	label_white.text = str(GameState.orders[order_number][3])
-	label_yellow.text = str(GameState.orders[order_number][4])
+	label_blue.text = str(order_resource.fruit_requirements[0])
+	label_brown.text = str(order_resource.fruit_requirements[1])
+	label_red.text = str(order_resource.fruit_requirements[2])
+	label_white.text = str(order_resource.fruit_requirements[3])
+	label_yellow.text = str(order_resource.fruit_requirements[4])
