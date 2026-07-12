4ff6be836d44ee9d1d14a8a644a48a5d165f6641
CookieFinder23 <190116541+CookieFinder23@users.noreply.github.com>
2026-07-12 12:42:03 -0400

    weighted random storm steal

--- enemies/take_item/take_item.gd
+++ enemies/take_item/take_item.gd
@@ -1,24 +1,13 @@
 extends Area2D
 
-@onready var boat = $"../../../Boat"
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _on_body_entered(body):
 	if GameState.in_restaurant:
 		return
-	if body == boat:
+	if body == GameState.boat:
 		var valid_idx = []
 		var i = 0
 		for f_count in GameState.inventory:
-			if f_count > 0:
+			for j in range(f_count):
 				valid_idx.append(i)
 			i += 1
 		if len(valid_idx) == 0:
