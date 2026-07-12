67ae8ae2cf1af5cc2c7b29de75155534e4b1833d
MrPuffyWuff <jasper13.com@gmail.com>
2026-07-12 17:18:39 -0400

    Restaurant UI

--- player/restaurant/restaurant_ui.gd
+++ player/restaurant/restaurant_ui.gd
@@ -7,18 +7,29 @@ extends Node2D
 
 func _ready():
 	GameState.restaurant_ui = self
+	disable_lid()
 
 func _on_button_pressed():
 	blend()
 
 func blend():
+	enable_lid()
 	blend_sound.playSound()
 	var blended = [0,0,0,0,0]
 	for body in $Area2D.get_overlapping_bodies():
 		blended[body.fruit_type] += 1
-		body.queue_free()
+		if body is RigidBody2D:
+			body.apply_impulse(Vector2(0,-100))
 	GameState.check_order(blended)
 
 func _on_exit_door_pressed():
 	exit_sound.playSound()
 	GameState.exit_restaurant()
+
+func enable_lid():
+	$Lid.visible = true
+	$Lid/CollisionShape2D.disabled = false
+
+func disable_lid():
+	$Lid.visible = false
+	$Lid/CollisionShape2D.disabled = true
