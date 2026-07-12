661e73ba453f6bd56e62c8ebbbb4da2b13415a49
MrPuffyWuff <jasper13.com@gmail.com>
2026-07-12 12:30:13 -0400

    Storm fixes

--- enemies/take_item/take_item.gd
+++ enemies/take_item/take_item.gd
@@ -12,7 +12,17 @@ func _process(delta):
 	pass
 
 func _on_body_entered(body):
+	if GameState.in_restaurant:
+		return
 	if body == boat:
-		GameState.inventory[randi_range(0,4)] -= 1
+		var valid_idx = []
+		var i = 0
+		for f_count in GameState.inventory:
+			if f_count > 0:
+				valid_idx.append(i)
+			i += 1
+		if len(valid_idx) == 0:
+			return
+		GameState.inventory[valid_idx[randi_range(0,len(valid_idx) - 1)]] -= 1
 		GameState.emit_signal("inventory_modified")
 		print("shouldve stolen one")
