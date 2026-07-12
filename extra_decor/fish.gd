c5e506bc201f3c28ff1cdfefd0ea5dffb1d1f0e3
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-07-12 10:47:35 -0400

    fish made

---
+++ extra_decor/fish.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var velocity = Vector2(0,0)
+
+func _ready():
+	rotation = randi_range(0,1) * PI
+
+func _process(delta):
+	if velocity:
+		position += velocity * delta
+
+func _on_body_entered(body):
+	if velocity:
+		return
+	velocity = (global_position.direction_to(body.global_position)).orthogonal() * 150 * (randi_range(0,1)*2-1)
+	look_at(global_position + velocity)
+	var tween = get_tree().create_tween()
+	tween.tween_property($Sprite2D, "modulate", Color(1.0, 1.0, 1.0, 0.0), 1)
+	tween.tween_callback(queue_free)
