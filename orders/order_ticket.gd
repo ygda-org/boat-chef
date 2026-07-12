7dc066fa34d29ad98a4fa6aa3a234b0e3c40ea28
MrPuffyWuff <jasper13.com@gmail.com>
2026-07-11 20:56:31 -0400

    colors

--- orders/order_ticket.gd
+++ orders/order_ticket.gd
@@ -24,22 +24,37 @@ var colors = [
 	Vector3(238,214,132),
 ]
 
+func get_gradient_colors() -> Array[Color]:
+	var child_idx = 0
+	var fruit_idx = 0
+	var base_color := Vector3.ZERO
+	
+	var counts : Array[int] = [0,0,0,0,0]
+	
+	for req in order_resource.fruit_requirements:
+		for i in range(req):
+			base_color += colors[fruit_idx]
+			child_idx += 1
+			#counts[fruit_idx]
+		fruit_idx += 1
+	base_color = base_color/child_idx
+	var color : Color = Color(base_color.x/255.0, base_color.y/255.0, base_color.z/255.0)
+	return [color, Color(1.0,1.0,1.0)]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var child_idx = 0
 	var fruit_idx = 0
-	var base_color := Vector3.ZERO
 	for req in order_resource.fruit_requirements:
 		for i in range(req):
 			$Fruits.get_child(child_idx).texture = fruits[fruit_idx]
 			$Fruits.get_child(child_idx).visible = true
-			base_color += colors[fruit_idx]
 			child_idx += 1
 		fruit_idx += 1
-	base_color = base_color/child_idx
-	var color : Color = Color(base_color.x/255.0, base_color.y/255.0, base_color.z/255.0)
-	$Fill.texture.gradient.set_color(0, color)
-	$Fill.texture.gradient.set_color(1, Color(1.0, 1.0, 1.0, 1.0))
+	var gradient_colors : Array[Color] = get_gradient_colors()
+	$Fill.texture.gradient = Gradient.new()
+	$Fill.texture.gradient.set_color(0, gradient_colors[0])
+	$Fill.texture.gradient.set_color(1, gradient_colors[1])
 	$TimeBar.max_value = order_resource.order_dur
 	$TimeBar.value = $TimeBar.max_value
 	
