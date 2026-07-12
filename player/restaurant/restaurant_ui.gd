0d0da50fe537441bf60647e57e33c60ae8002463
MrPuffyWuff <jasper13.com@gmail.com>
2026-07-12 17:32:29 -0400

    Bllender Functional Again

--- player/restaurant/restaurant_ui.gd
+++ player/restaurant/restaurant_ui.gd
@@ -16,9 +16,16 @@ func blend():
 	var blended = [0,0,0,0,0]
 	for body in $Area2D.get_overlapping_bodies():
 		blended[body.fruit_type] += 1
-		if body is RigidBody2D:
-			body.apply_impulse(Vector2(0,-100))
 	GameState.check_order(blended)
+	for i in range(4):
+		for body in $Area2D.get_overlapping_bodies():
+			if body is RigidBody2D:
+				body.apply_impulse(Vector2(0,-60))
+		await get_tree().create_timer(0.3).timeout
+	for body in $Area2D.get_overlapping_bodies():
+		body.queue_free()
+	await get_tree().create_timer(0.3).timeout
+	disable_lid()
 
 func _on_exit_door_pressed():
 	exit_sound.playSound()
