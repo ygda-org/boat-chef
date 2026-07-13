33da5b5ce1ad5234e62fbda227956c753c980df0
Aaron Thapa <notsmart98@gmail.com>
2026-07-12 22:12:10 -0400

    make music crossfade

--- main/music.gd
+++ main/music.gd
@@ -2,12 +2,17 @@ extends Node
 
 @onready var malik_theme = $MalikTheme
 @onready var erina_theme = $ErinaTheme
+@onready var animation_player = $AnimationPlayer
 
 var music = randi_range(0,1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	play_song()
+	music = randi_range(0,1)
+	if music == 0:
+		malik_theme.playSound()
+	else:
+		erina_theme.playSound()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,19 +21,19 @@ func _process(delta):
 
 
 func _on_malik_theme_finished():
-	finished()
+	malik_theme.play()
+	malik_theme.seek(0.0)
+	if randi_range(0, 1):
+		erina_theme.play()
+		animation_player.play_backwards("erina_out")
+		await get_tree().create_timer(5.0).timeout
+		malik_theme.stop()
 
 func _on_erina_theme_finished():
-	finished()
-	
-func play_song():
-	if music == 0:
-		malik_theme.playSound()
-		await get_tree().create_timer(3.0).timeout
-	else:
-		erina_theme.playSound()
-		await get_tree().create_timer(3.0).timeout
-
-func finished():
-	music = randi_range(0,1)
-	play_song()
+	erina_theme.play()
+	erina_theme.seek(39.18)
+	if randi_range(0, 1):
+		malik_theme.play()
+		animation_player.play("erina_out")
+		await get_tree().create_timer(5.0).timeout
+		erina_theme.stop()
